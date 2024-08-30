6db618e2f4e01351d21824a66a3c64c433e34aa7
4gvzz <gab.agustin.412@gmail.com>
2024-08-30 19:00:41 -0300

    cree el main y la clase envio

---
+++ main.py
@@ -0,0 +1,29 @@
+from envio import Envio
+
+
+def cargar_envios(registros):
+    archivo = open("envios-tp3.txt")
+    contenido = archivo.readlines()
+    archivo.close()
+
+    contandor_line = 0
+    for line in contenido:
+        contandor_line += 1
+
+        if contandor_line == 1:
+            continue
+
+        envio = Envio()
+
+        envio.obtener_codigo_postal(line)
+        envio.obtener_direccion(line)
+        envio.obtener_tipo_envio(line)
+        envio.obtener_forma_pago(line)
+
+        envio.mostrar_info()
+
+
+if __name__ == "__main__":
+    registros = []
+
+    cargar_envios(registros)
