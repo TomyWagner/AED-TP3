c0b58a79c9e7d68d0d21ad9638669226164e2af7
4gvzz <gab.agustin.412@gmail.com>
2024-09-01 19:11:25 -0300

    Opción 2 del menú casi terminada

--- envio.py
+++ envio.py
@@ -8,15 +8,3 @@ class Envio:
     def mostrar_info(self):
         print(f"{self.codigo_postal} {self.direccion} {
               self.tipo_envio} {self.forma_pago}")
-
-    def obtener_codigo_postal(self, line):
-        self.codigo_postal = line[0:8]
-
-    def obtener_direccion(self, line):
-        self.direccion = line[9:28]
-
-    def obtener_tipo_envio(self, line):
-        self.tipo_envio = line[29]
-
-    def obtener_forma_pago(self, line):
-        self.forma_pago = line[30]
--- main.py
+++ main.py
@@ -22,11 +22,10 @@ def cargar_envios():
             continue
 
         envio = Envio()
-
-        envio.obtener_codigo_postal(linea)
-        envio.obtener_direccion(linea)
-        envio.obtener_tipo_envio(linea)
-        envio.obtener_forma_pago(linea)
+        envio.codigo_postal = linea[0:8]
+        envio.direccion = linea[9:28]
+        envio.tipo_envio = int(linea[29])
+        envio.forma_pago = int(linea[30])
 
         envios.append(envio)
 
@@ -34,11 +33,10 @@ def cargar_envios():
 
 
 def menu():
-    """ esta función sirve de menú, verifica el valor ingresado
+    """ esta función muestra el menú, verifica el valor ingresado
          y lo devuelve. (Solo muestra las diferentes operaciones
          que puede hacer el programa).
     """
-    # Debe haber una mejor forma de hacer esto.
 
     print("1. Obtener registros desde un archivo")
     print("2. Cargar un envío")
@@ -46,18 +44,20 @@ def menu():
     print("4. Salir")
     opc = input("ingrese una opción: ")
 
+    # verificación -----
     if opc == "":
         print("Seleccione una opción valida.")
         return
 
-    if opc.isalpha():
+    elif opc.isalpha():
         print("La opción debe ser un valor numérico")
         return
 
-    elif opc.isnumeric():
+    elif opc.isdigit():
         if int(opc) <= 0 or int(opc) >= 5:
             print("Seleccione una opción valida.")
             return
+    # -----------------
 
     return int(opc)
 
@@ -76,29 +76,53 @@ if __name__ == "__main__":
                 envios = cargar_envios()
                 print("\nRegistros cargados\n")
 
-            # si el arreglo no esta vacío
+            # si el arreglo no esta vacío,
             # cancelar la operación o volver a cargar
             else:
                 x = input("Seguro que quieres borrar los registros? (Ss/Nn): ")
-                print("\n")
                 if x in 'Ss':
+                    print("\nRegistros cargados\n")
                     envios = cargar_envios()
-                print("\nRegistros cargados\n")
                 if x in 'Nn':
                     print("\nOperación cancelada\n")
 
         elif opc == 2:
-            # TODO cargar por teclado los datos de envío, aplicando procesos
-            # de validación para los campos tipo de envío y forma de pago,
-            # agregar un objeto con esos datos al arreglo.
-            # Cada vez que se elija esta opción, EL NUEVO REGISTRO DEBE
-            # AGREGARSE AL FINAL DEL ARREGLO.
-            #
-            # Si se elige esta opción, los datos anteriores
-            # deben ser MANTENIDOS en el arreglo sin importar si fueron
-            # originalmente cargados desde el archivo o fueron cargados
-            # manualmente desde esta misma opción
-            print("\n\nSeleccionó la opción número 2\n\n")
+            print("Carga de datos")
+
+            codigo_postal = input("Ingrese el código postal: ")
+            direccion = input("Ingrese la dirección física: ")
+
+            # valida el tipo de envío y la forma de pago --------
+
+            # FIXME hay un error al poner números y letras juntos
+            # creo que es porque la función isalpha devuelve True
+            # solo si todos los caracteres son letras, lo mismo pasa
+            # con isdigit.
+            tipo_envio = input("Ingrese el tipo de envío (del 0 al 6): ")
+            while tipo_envio.isalpha() or (tipo_envio.isdigit() and (int(tipo_envio) <= -1 or int(tipo_envio) >= 7)):
+                print("Error")
+                tipo_envio = input("Ingrese el tipo de envío (del 0 al 6): ")
+
+            forma_pago = input("Ingrese el tipo de envío (del 1 al 2): ")
+            while forma_pago.isalpha() or (forma_pago.isdigit() and (int(forma_pago) <= 0 or int(forma_pago) >= 3)):
+                print("Error")
+                forma_pago = input("Ingrese el tipo de envío (del 1 al 2): ")
+            # ---------------------------------------------------
+
+            envio = Envio()
+            envio.codigo_postal = codigo_postal
+            envio.direccion = direccion
+            envio.tipo_envio = tipo_envio
+            envio.forma_pago = forma_pago
+
+            # agrega el envío al final del arreglo
+            envios.append(envio)
+
+            # código de prueba
+            print("\n\n")
+            for e in envios:
+                e.mostrar_info()
+
         elif opc == 3:
             # TODO muestra los objetos del arreglo, ordenados por
             # código postal, de menor a mayor.
